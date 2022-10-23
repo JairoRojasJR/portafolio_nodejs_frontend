18611d59c1eb500382498c9138019343bdd22aa9
Jairo Rojas <jairorojasofficial@gmail.com>
2022-10-23 17:53:24 -0500

    Probando next-seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
+import { NextSeo } from 'next-seo'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <NextSeo
+      title="Jayo Web"
+      titleTemplate="Jayo Web"
+      defaultTitle="Jayo Web"
+      description="Soy Jairo Rojas y soy desarrollador web"
+      canonical="https://www.jayoweb.com/"
+      openGraph={{
+        url: "https://www.jayoweb.com/",
+        title: "Jayo Web",
+        description: "Soy Jairo Rojas y soy desarrollador web",
+        images: [
+          {
+            url: "/favicon.ico",
+            width: 800,
+            height: 420,
+            alt: "Jayo Web",
+          },
+        ],
+      }}
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
