e6a7cc23d9511beabea79b292168f284b52129c8
Jairo Rojas <jairorojasofficial@gmail.com>
2022-11-25 04:55:22 -0500

    Implementacion del titulo principal

--- pages/index.js
+++ pages/index.js
@@ -13,19 +13,28 @@ export default function Home() {
       </Head>
       <Background />
       <main className='main pr h100vh w100p'>
-        <h1>Jayo Web - Developer web</h1>
+        <h1>Jairo Rojas - Desarrollador Web</h1>
         <Nav />
         <Presentation />
       </main>
       <style jsx>{`
-          main {
+        main {
+          display: flex;
+          flex-direction: column;
           z-index: 200;
           max-width: 1200px;
           margin: 0 auto;
+          position: relative;
+          align-items: center;
+          justify-content: center;
         }
 
         h1 {
           position: absolute;
+          background-color: #0009;
+          border-radius: .5rem;
+          padding: .5rem 1rem;
+          text-align: center;
         }
       `}</style>
     </div>
